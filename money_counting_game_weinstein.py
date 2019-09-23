50904dc6ac4133146f6917e38b4c6c304a5b62ed
David Weinstein <weinstein.dev@gmail.com>
2019-09-23 19:35:02 -0400

    formatting and spelling

--- money_counting_game_weinstein.py
+++ money_counting_game_weinstein.py
@@ -1,5 +1,5 @@
 """
-This program is a change-counting game where the goal is for the suer to enter the number of coins to make exactly one dollar.
+This program is a change-counting game where the goal is for the user to enter the number of coins to make exactly one dollar.
 Constants for penny, nickel, dime, and quarter values, and total pennies in dollar.
 
 Programmer: David Weinstein
@@ -11,11 +11,12 @@ Pseudocode:
 2. Initial user input for number of coins
 3. Check if any negative integers input
     3.1 If any negative intgers, have user choose new inputs
-4. If no negative integers, continue on to check if total value of coins > one dollar
+4. Value is set to the sum of coin amount time coin value
+5. If no negative integers, continue on to check if total value of coins > one dollar
     4.1 If total value of coins > one dollar, print "the amount was more than one dollar"
-5. If total value of coins < one dollar
+6. If total value of coins < one dollar
     5.1 Print "the amount was less than one dollar"
-6. Else print winning message, total value of coins must equal 0
+7. Else print winning message, total value of coins must equal one dollar
 """
 
 # Define constants.
@@ -39,12 +40,13 @@ while pennies < 0 or nickels < 0 or dimes < 0 or quarters < 0:
     dimes = int(input("Enter the number of dimes: "))
     quarters = int(input("Enter the number of quarters: "))
 
-# Check if more than one dollar
-if pennies * PENNY_VALUE + nickels * NICKEL_VALUE + dimes * DIME_VALUE + quarters * QUARTER_VALUE > PENNIES_IN_DOLLAR:
+# Value of coins if al positive integers
+value = pennies * PENNY_VALUE + nickels * NICKEL_VALUE + dimes * DIME_VALUE + quarters * QUARTER_VALUE
+
+# Check if more than one dollar, else if check if less than one dollar, else print winning message
+if value > PENNIES_IN_DOLLAR:
     print("Sorry, the amount you entered was more than one dollar.")
-# Check if less than one dollar
-elif pennies * PENNY_VALUE + nickels * NICKEL_VALUE + dimes * DIME_VALUE + quarters * QUARTER_VALUE < PENNIES_IN_DOLLAR:
+elif value < PENNIES_IN_DOLLAR:
     print("Sorry, the amount you entered was less than one dollar.")
-# Else print winning message
 else:
     print("Congratulations!\nThe amount you entered was exactly one dollar!\nYou win the game!")
--- wifi_diagnostics_tree_weinstein.py
+++ wifi_diagnostics_tree_weinstein.py
@@ -6,11 +6,11 @@ Date: 9/23/2019
 Filename: wifi_diagnostics_tree_weinstein.py
 
 Pseudocode:
-1. Set answer vairable equal to no, to use with while loop.
+1. Set answer variable equal to no, to use with while loop.
 2. Make list of possible diagnosis.
 3. Set variable for user input of "Did that fix the problem?".
 4. Set count to 0 in order to index through the diagnostics.
-5. While answer is set to "no"
+5. While answer is equal to "no"
     5.1 Print the current diagnosis from list.
     5.2 Ask user if that fixed the problem.
     5.3 Iterate count by one.
@@ -21,20 +21,21 @@ Pseudocode:
 # Set answer to no for while loop
 answer = "no"
 
-# List of diagnostic steps
+# List of diagnostic steps, and final step if nothing works.
 diagnosis = ["Reboot the computer and try to connect.",
     "Reboot the router and try to connect.",
     "Make sure the cables between the router & modem are plugged in firmly.",
     "Move the router to a new loaction and try to connect."
  ]
+finalStep = "Get a new router."
 
- # Variable for user input string
+# Variable for user input string
 fixInput = "Did that fix the problem? "
 
-# Count to be iterated by one to use for indexing
+# Count to be iterated by one to use for indexing through list
 count = 0
 
-# While loop will loop through diagnosis list until a soluion is found or we come to final solution.
+# While loop will loop through diagnosis list until a soluion is found or we come to the final step.
 while answer == "no" and count < len(diagnosis):
     print(diagnosis[count])
     answer = input(fixInput)
@@ -42,4 +43,4 @@ while answer == "no" and count < len(diagnosis):
     if answer == "yes":
         print("Glad to be of help.")
     elif count == len(diagnosis) and answer == "no":
-        print("Get a new router.")
+        print(finalStep)
