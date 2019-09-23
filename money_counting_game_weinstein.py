bf760bb08f91c14d41d3ce4bd3d010d0b47623dc
David Weinstein <weinstein.dev@gmail.com>
2019-09-23 15:53:28 -0400

    finished money counting game

--- money_counting_game_weinstein.py
+++ money_counting_game_weinstein.py
@@ -7,6 +7,7 @@ Date: 9/23/2019
 File Name: money_counting_game_weinstein.py
 
 Pseudocode:
+1. 
 """
 
 # Define constants.
@@ -22,3 +23,18 @@ nickels = int(input("Enter the number of nickels: "))
 dimes = int(input("Enter the number of dimes: "))
 quarters = int(input("Enter the number of quarters: "))
 
+# Main loop for game
+# error message for negative integer 
+if pennies < 0 or nickels < 0 or dimes < 0 or quarters < 0:
+    print("The number should be a positive integer!")
+# Check if more than one dollar
+elif pennies * PENNY_VALUE + nickels * NICKEL_VALUE + dimes * DIME_VALUE + quarters * QUARTER_VALUE > PENNIES_IN_DOLLAR:
+    print("Sorry, the amount you entered was more than one dollar.")
+# Check if less than one dollar
+elif pennies * PENNY_VALUE + nickels * NICKEL_VALUE + dimes * DIME_VALUE + quarters * QUARTER_VALUE < PENNIES_IN_DOLLAR:
+    print("Sorry, the amount you entered was less than one dollar.")
+# print winning message if exactly one dollar.
+else:
+    print("Congratulations!\nThe amount you entered was exactly one dollar!\nYou win the game!")
+
+    
